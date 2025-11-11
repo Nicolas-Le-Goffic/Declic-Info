fcbd32e31114ad3c185a453c47b56098d38b76cf
Julie-Denes <julie.denes29@gmail.com>
2025-11-11 16:04:22 +0100

    Ajout de la classe Produit

--- Declic-InfoBO/Class1.cs
+++ Declic-InfoBO/Class1.cs
@@ -6,7 +6,61 @@ using System.Threading.Tasks;
 
 namespace Declic_InfoBO
 {
-    public class Class1
+    public class Produit
     {
+        protected int id;
+        protected string libelle;
+        protected string categorie;
+        protected decimal prix;
+
+        public Produit() { }
+
+        public Produit(int id, string libelle, string categorie, decimal prix)
+        {
+            this.id = id;
+            this.libelle = libelle;
+            this.categorie = categorie;
+            this.prix = prix;
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public string getLibelle()
+        {
+            return libelle;
+        }
+
+        public string getCategorie()
+        {
+            return categorie;
+        }
+
+        public decimal getPrix()
+        {
+            return prix;
+        }
+
+        public void setId(int id)
+        {
+            this.id = id;
+        }
+
+        public void setLibelle(string libelle)
+        {
+            this.libelle = libelle;
+        }
+
+        public void setCategorie(string categorie)
+        {
+            this.categorie = categorie;
+        }
+
+        public void setPrix(decimal prix)
+        {
+            this.prix = prix;
+        }
     }
 }
