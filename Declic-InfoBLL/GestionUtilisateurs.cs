c6abb2606bcef8e3a06c4605899473affda63702
Kecy Platel <kecy.platel@gmail.com>
2025-11-13 12:04:51 +0100

    ajout de l'authentification utilisateur dans la couche DAL et BLL

---
+++ Declic-InfoBLL/GestionUtilisateurs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Declic_InfoDAL;
+
+namespace Declic_InfoBLL
+{
+    public class GestionUtilisateurs
+    {
+            private static GestionUtilisateurs uneGestionUtilisateurs; // objet BLL
+
+            // Accesseur en lecture
+            public static GestionUtilisateurs GetGestionUtilisateurs()
+            {
+                if (uneGestionUtilisateurs == null)
+                {
+                    uneGestionUtilisateurs = new GestionUtilisateurs();
+                }
+                return uneGestionUtilisateurs;
+            }
+
+            // Définit la chaîne de connexion grâce à la méthode SetchaineConnexion de la DAL
+            public static void SetchaineConnexion(ConnectionStringSettings chset)
+            {
+                string chaine = chset.ConnectionString;
+                ConnexionBD.GetConnexionBD().SetchaineConnexion(chaine);
+            }
+            // Méthode qui renvoit une List d'objets Utilisateur en faisant appel à la méthode GetUtilisateurs() de la DAL
+            public static bool VerifUtilisateur(string login, string mdp)
+            {
+                return UtilisateurDAO.GetunUtilisateurDAO().VerifUtilisateur(login,mdp);
+            }
+
+        }
+    }
---
+++ Declic-InfoDAL/UtilisateurDAO.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+
+namespace Declic_InfoDAL
+{
+    public class UtilisateurDAO
+    {
+        private static UtilisateurDAO unUtilisateurDAO;
+        // Accesseur en lecture, renvoi une instance
+        public static UtilisateurDAO GetunUtilisateurDAO()
+        {
+            if (unUtilisateurDAO == null)
+            {
+                unUtilisateurDAO = new UtilisateurDAO();
+            }
+            return unUtilisateurDAO;
+        }
+
+        // Cette méthode vérifie si un utilisateur existe
+        public static bool VerifUtilisateur(string login, string mdp)
+        {
+            bool existe=false;
+            // Connexion à la BD
+            SqlConnection maConnexion = ConnexionBD.GetConnexionBD().GetSqlConnexion();
+
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = maConnexion;
+            cmd.CommandText = cmd.CommandText = "SELECT * FROM Utilisateur WHERE nom_utilisateur =  " + login + "AND mot_de_passe_utilisateur = " + mdp;
+            SqlDataReader monReader = cmd.ExecuteReader();
+
+
+            int nbUtilisateur = (int)cmd.ExecuteScalar();
+            if (nbUtilisateur == 1)
+            {
+                existe = true;
+            }
+
+            return existe;
+
+
+            // Fermeture de la connexion
+            maConnexion.Close();
+
+        }
+
+
+    }
+}
+
