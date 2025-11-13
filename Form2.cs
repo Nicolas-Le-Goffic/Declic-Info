d28236a132600b269e3dad3fa522dbe99bdad8dc
Nicolas Le-Goffic <legoffic.nicolas@gmail.com>
2025-11-13 10:23:44 +0100

    Références entre les couches de données

--- Form2.cs
+++ Form2.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Declic_InfoBLL;
+using Declic_InfoBO;
+using System.Configuration;
 
 namespace Declic_Info
 {
